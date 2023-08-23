dc5dd95adb6a2911218d9c5576cd7d5f68d08cb9
barlin41k <132909893+barlin41k@users.noreply.github.com>
2023-08-23 17:07:39 +0300

    0.2.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='barladb',
-    version='0.2.0',
+    version='0.2.1',
     description='A very easy local database based on JSON',
     packages=['barladb'],
     author_email='sasaigrypocta@gmail.com',
@@ -10,8 +10,16 @@ setup(
     zip_safe=False,
     long_description='''
 # Изменения:
-- Структура `db.py` была изменена на класс с функцией `__init__`
-- Удалена зависимость: `colorama`
+## [БЕТА] Новая функция: Логирование
+- Каждое действие будет записываться каждый день в новый файл log_ДД.ММ.ГГ.txt
+- Как его подключить, можно узнать в лёгком примере использования ниже
+- При ошибках, сообщайте в Discord: **barlin41k**
+
+- Исправлены баги, недочёты
+
+# ToDo
+- Добавить обработку исключений на команды `columns`, `remove_column`, `search`
+- Исправить все возможные недочёты
 
 
 # Что такое barlaDB?
@@ -20,10 +28,11 @@ setup(
 # Лёгкий пример использования
 ```python
 from barladb import db #Импортирование функций БД
-from barladb import config #Импортируем конфиг для того чтобы подключить логирование действий
-config.debug = True #Включение логирования
+from barladb import config #Импортируем конфиг для того чтобы подключить дебаг и лог действий
+config.debug = True #Включение дебага
+config.log = True #Включение лога
 
-db = db.BarlaDB() #Создание экземпляра класса
+db = db.BarlaDB()
 data = db.get("example") #Достаем содержимое БД и сохраняем его в переменную data. Заметьте, что мы не пишем расширение (.json)
 #Также, если ваш файл находится в другой папке, всего-лишь требуется прописать другой путь, к примеру
 #db.get("path/to/file/example")
